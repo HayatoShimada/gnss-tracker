eddd91ba09e42aa44505671a02fa289c19432329
HayatoShimada <hayato_shimada@kanamori-system.co.jp>
2025-03-16 22:50:02 +0900

    client

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
-// Google Maps は SSR に対応していないため、クライアントサイドでのみ読み込む
+// クライアントコンポーネントとして MapPage を読み込む
 const MapPage = dynamic(() => import("../components/MapPage"), { ssr: false });
 
 export default function Map() {
